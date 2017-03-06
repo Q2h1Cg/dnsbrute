4e4ce7aa76a4ba385ab8a9800c92b31e7c50fe38
Chu <root@sh3ll.me>
2017-03-06 18:55:54 +0800

    gofmt

--- dns/depth.go
+++ dns/depth.go
@@ -4,14 +4,13 @@ import "strings"
 
 var (
 	rootDomain string
-	depthLimit int	= 3
+	depthLimit int = 3
 )
 
 // SetRootDomain 设置根域名
 func SetRootDomain(root string) {
 	rootDomain = root
 }
-
 
 // SetDepthLimit 设置爆破域名深度
 func SetDepthLimit(limit int) {
@@ -22,7 +21,6 @@ func SetDepthLimit(limit int) {
 func DomainDepth(subdomain string) int {
 	return strings.Count(strings.Trim(subdomain, rootDomain), ".")
 }
-
 
 // UnderLimit 是否小于深度限制
 func UnderLimit(subdomain string) bool {
