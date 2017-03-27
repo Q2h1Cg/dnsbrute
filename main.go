c51eaad58a516bfd251e50817f52643f2f86c355
Chu <root@sh3ll.me>
2017-03-28 00:55:54 +0800

    主程序

---
+++ main.go
@@ -0,0 +1,145 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"os"
+	"strings"
+	"sync"
+	"time"
+
+	"github.com/chuhades/dnsbrute/dns"
+	"github.com/chuhades/dnsbrute/log"
+)
+
+var versionNumber = "1.0#dev"
+
+type SubDomainRecursiveCounter struct {
+	counter    uint
+	recursived bool
+}
+
+func main() {
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, "Usage: \n  %s [Options] {target}\n\nOptions\n", os.Args[0])
+		flag.PrintDefaults()
+	}
+
+	debug := flag.Bool("debug", false, "Show debug information")
+	requestDelay := flag.Duration("delay", time.Millisecond, "delay between each DNS request")
+	threads := flag.Int("threads", 10, "number of DNS client(s)")
+	dictFile := flag.String("dict", "dict/53683.txt", "dict file")
+	target := flag.String("target", "", "target")
+	version := flag.Bool("version", false, "Show program's version number and exit")
+	flag.Parse()
+
+	// show program's version number and exit
+	if *version {
+		fmt.Println(versionNumber)
+		os.Exit(0)
+	}
+
+	if *target == "" {
+		fmt.Println("no target")
+		os.Exit(1)
+	}
+
+	// set log level to log.DEBUG
+	if *debug {
+		log.SetLevel(log.DEBUG)
+	}
+
+	// set delay between each DNS request
+	dns.RequestDelay = *requestDelay
+
+	// set depth limit
+	dns.SetRootDomain(*target)
+
+	// pan analytic
+	dns.AnalyzePanAnalytic()
+
+	// query and records
+	sent := make(map[string]struct{})
+	query := make(chan string, 100000)
+	queryUnderLimit := func() chan<- string {
+		ch := make(chan string, 1000)
+		go func() { ch <- *target }()
+		go func() {
+			for domain := range ch {
+				fd, err := os.Open(*dictFile)
+				if err != nil {
+					log.Fatal("Error while open dict:", err)
+				}
+
+				scanner := bufio.NewScanner(fd)
+				for scanner.Scan() {
+					sub := strings.TrimSpace(scanner.Text())
+					if sub != "" {
+						subdomain := sub + "." + domain
+						if _, ok := sent[subdomain]; !ok {
+							query <- subdomain
+						}
+					}
+				}
+
+				if err := scanner.Err(); err != nil {
+					log.Fatal("Error while read dict:", err)
+				}
+				fd.Close()
+			}
+		}()
+		return ch
+	}()
+	records := make(chan dns.DNSRecord)
+
+	// query over api
+	log.Info("querying over API")
+	for domain := range dns.QueryOverAPI(*target) {
+		if _, ok := sent[domain]; !ok {
+			query <- domain
+		}
+	}
+
+	// clients
+	wg := sync.WaitGroup{}
+	recursiveCounter := map[string]*SubDomainRecursiveCounter{}
+	clients := []dns.DNSClient{}
+	for i := 0; i < *threads; i++ {
+		clients = append(clients, dns.NewClient())
+	}
+	// drive all client
+	for _, c := range clients {
+		go func(client dns.DNSClient) {
+			for domain := range query {
+				client.Query <- domain
+			}
+		}(c)
+		wg.Add(1)
+		go func(client dns.DNSClient) {
+			defer wg.Done()
+			for record := range client.Record {
+				records <- record
+			}
+		}(c)
+	}
+	go func() {
+		wg.Wait()
+		close(records)
+	}()
+
+	for record := range records {
+		log.Info(record)
+		parentDomain := dns.ParentDomain(record.Domain)
+		if _, ok := recursiveCounter[parentDomain]; !ok {
+			recursiveCounter[parentDomain] = &SubDomainRecursiveCounter{}
+		}
+		recursiveCounter[parentDomain].counter++
+		// 阈值：5
+		// 如果某子域名在 API 的查询结果中子域名大于阈值，对其进行字典爆破
+		if parentDomain != *target && recursiveCounter[parentDomain].counter > 5 && !recursiveCounter[parentDomain].recursived {
+			recursiveCounter[parentDomain].recursived = true
+			queryUnderLimit <- parentDomain
+		}
+	}
+}
